1b6a5a46a4a37f0b10368e7988b3bdc5b97ad4f2
IrinaSavosh <irina_savosh@mail.ru>
2023-03-24 21:29:30 +0300

    homework 1

---
+++ hw_41.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class hw_41 {
+   /*
+    * Пусть дан LinkedList с несколькими элементами. Реализуйте метод(не void), который вернет “перевернутый” список.
+    */
+    public static void main(String[] args) {
+          
+      LinkedList<String> fruit = new LinkedList<String>();
+      fruit.add("Apple");
+      fruit.add("Orange");
+      fruit.add("Mango");
+      fruit.add("Pineapple");
+      fruit.add("Banana");
+
+
+      System.out.println(fruit);
+      reverse(fruit);
+      System.out.println();
+      System.out.println(fruit);
+
+    }
+
+   public static LinkedList<String> reverse (LinkedList<String> array){
+      for (int i = 0; i < array.size(); i++) {
+         String element = array.getLast();
+         array.removeLast();
+         array.add(i, element);
+      }
+      return array;
+   }
+
+}
