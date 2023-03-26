b7c526d268f242ca87c3a5cdbb39b6dd9231500a
IrinaSavosh <irina_savosh@mail.ru>
2023-03-26 21:45:05 +0300

    homework 2

--- hw_42.java
+++ hw_42.java
@@ -1,5 +1,5 @@
 import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Scanner;
 
 public class hw_42 {
    /*
@@ -10,16 +10,45 @@ first() - возвращает первый элемент из очереди, 
 enqueue() - dequeue() - first()
     */
    public static void main(String[] args){
-      Queue<String> fruit = new LinkedList<String>();
-      fruit.add("Apple");
-      fruit.add("Orange");
-      fruit.add("Mango");
-      fruit.add("Pineapple");
-      fruit.add("Banana");
+      LinkedList<String> fruits = new LinkedList<String>();
+      fruits.add("Apple");
+      fruits.add("Orange");
+      fruits.add("Mango");
+      fruits.add("Pineapple");
+      fruits.add("Banana");
 
+      System.out.println(fruits);
+
+      String fruit = dequeue(fruits);
       System.out.println(fruit);
+      System.out.println(fruits);
 
-      // fruit.enqueue("gfh");
-      // System.out.println(fruit);
+      enqueue(fruits);
+      System.out.println(fruits);
+
+      fruit = first(fruits);
+      System.out.println(fruit);
+      System.out.println(fruits);
+   }
+   public static LinkedList<String> enqueue (LinkedList<String> array){
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Введите название фрукта: ");
+      String new_word = scan.nextLine();
+     
+      array.addLast(new_word);
+      scan.close();
+      return array;
+
+   }
+
+   public static String dequeue (LinkedList<String> array){
+      String word = array.removeFirst();   
+      array.removeFirst();
+      return word;
+   }
+
+   public static String first (LinkedList<String> array){
+      String word = array.getFirst();   
+      return word;
    }
 }
